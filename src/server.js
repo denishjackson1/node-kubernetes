6a6ce30f2652f25c9494498f4753bcff333baed8
denishjackson1 <denishkomakech@gmail.com>
2023-11-28 08:30:38 +0300

    production env

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 */
 
 app.get('/', (req, res) => {
-  res.json('This application is in Production Environment!');
+  res.json('This application is in Production Environment!-!');
 });
 
 app.get('/todos', (req, res) => {
