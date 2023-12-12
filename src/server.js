f3e957d176af61612473ce7145472ff26ae366b9
denishjackson1 <denishkomakech@gmail.com>
2023-12-12 15:13:42 +0300

    tag

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 */
 
 app.get('/', (req, res) => {
-  res.json('This application is in staging Environment V6!!!!');
+  res.json('This application is in staging Environment V7!!!!');
 });
 
 app.get('/todos', (req, res) => {
