b4c46a032cc8f39853bb40a05de8aab64f1cee78
denishjackson1 <denishkomakech@gmail.com>
2023-12-13 12:30:51 +0300

    using version

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 */
 
 app.get('/', (req, res) => {
-  res.json('This application is in staging Environment V10!!!!');
+  res.json('This application is in staging Environment v2!!!!');
 });
 
 app.get('/todos', (req, res) => {
